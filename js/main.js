70464b8bbc498198e0f232f2686895df2f95eae7
kotovaaa02 <103637037+kotovaaa02@users.noreply.github.com>
2022-05-18 15:03:21 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('.js-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
